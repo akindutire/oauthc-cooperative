4cd24ada0e2468a8f2fceb030b52e8caa7266c59
temitayopelumi <olalekantemitayo7@gmail.com>
2020-02-12 18:33:06 +0100

    'feedback_form'

---
+++ Feedback.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+         <html>
+         <head>
+
+             <title>Feedback</title>
+             <meta name="viewport" content="width=device-width, initial-scale=1">
+             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+             <style>
+             </style>
+
+         </head>
+
+         <body class="" style="font-family:'sans-serif' !important;">
+
+         
+             
+                <div class=" bg-danger">
+                        <div class="row">
+                            <form class="col-sm-12  col-md-6 offset-md-3 bg-white my-4 p-4" action='/.html'>
+                            <span> Contact us</span>
+                            <div></br></div>
+                            <div class="form-group">
+                                <label for="name">First-Name:</label>
+                                <input type="texr" class="form-control" placeholder="First name" id="firstname">
+                            </div>
+                            <div class="form-group">
+                                <label for="name">Last-Name:</label>
+                                <input type="text" class="form-control" placeholder="Last Name" id="lastname">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address:</label>
+                                <input type="email" class="form-control" placeholder="Enter email" id="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="subject">Subject:</label>
+                                <input type="text" class="form-control" placeholder="subject" id="subject">
+                            </div>
+                            <div class="form-group">
+                                <label for="message">Message:</label>
+                                <br>
+                                <textarea rows="4" cols="77" id="message"  name="message" placeholder="Please enter your comments..."></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-danger">Submit</button>
+                            </form>
+                        </div>
+                </div>    
+           
+            
+
+         <footer></footer>
+
+         </body>
+
+         </html>
+         
